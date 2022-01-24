0f2c568df27aeab9e63d62665035747ba5e4ff5c
timduran <durantimothy21@gmail.com>
2022-01-23 21:25:24 -0800

    adding new JATE

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -59,10 +59,10 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
-            }
-          }
-        }
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
